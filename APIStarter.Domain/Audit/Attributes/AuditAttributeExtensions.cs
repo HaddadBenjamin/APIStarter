06fba3578c59440f88c20c82707a6664fc746644
Firefouks <haddad.b.pro@gmail.com>
2020-10-15 14:22:56 +0200

    fix

--- APIStarter.Domain/Audit/Attributes/AuditAttributeExtensions.cs
+++ APIStarter.Domain/Audit/Attributes/AuditAttributeExtensions.cs
@@ -7,6 +7,6 @@ namespace APIStarter.Domain.Audit.Attributes
     public static class AuditAttributeExtensions
     {
         public static bool ShouldAudit(this MemberInfo memberInfo) => !(memberInfo is null || memberInfo.GetCustomAttributes<ShallNotAuditAttribute>(true).Any());
-        public static bool ShouldAudit(this Type type) => !(type is null && type.GetCustomAttributes<ShallNotAuditAttribute>(true).Any());
+        public static bool ShouldAudit(this Type type) => !(type is null || type.GetCustomAttributes<ShallNotAuditAttribute>(true).Any());
     }
 }
