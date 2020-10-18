577deb1495f1e0af544ef52773d1dd76f71d3107
Firefouks <haddad.b.pro@gmail.com>
2020-10-18 21:12:40 +0200

    read items queries

--- Read Model/ReadModel.Infrastructure.WriteModel/SqlQueries/SqlQueries.cs
+++ Read Model/ReadModel.Infrastructure.WriteModel/SqlQueries/SqlQueries.cs
@@ -2,6 +2,21 @@
 {
     public static class WriteModelSqlQueries
     {
-        public static string SearchItems = "";
+        public static string SearchItems = @"
+            --DECLARE @Id UNIQUEIDENTIFIER
+
+            DROP TABLE IF EXISTS  #TempItems
+
+            SELECT Id, Name
+            INTO #TempItems
+            FROM Items
+            WHERE @Id IS NULL OR @Id = Id
+
+            ALTER TABLE #TempItems ADD PRIMARY KEY (ID);
+
+            SELECT * FROM #TempItems
+            SELECT il.Id, il.ItemId, il.Name 
+            FROM ItemLocation il
+            INNER JOIN #TempItems ti ON il.ItemId = ti.Id";
     }
 }
