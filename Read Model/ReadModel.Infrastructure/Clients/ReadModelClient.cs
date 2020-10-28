a64292ef054a69d7cedc1d5d33c39915ce870966
Firefouks <haddad.b.pro@gmail.com>
2020-10-28 22:39:41 +0100

    clean client

--- Read Model/ReadModel.Infrastructure/Clients/ReadModelClient.cs
+++ Read Model/ReadModel.Infrastructure/Clients/ReadModelClient.cs
@@ -1,11 +1,7 @@
 ﻿using System;
 using Nest;
-using ReadModel.Domain;
 using ReadModel.Domain.Clients;
 using ReadModel.Domain.Configurations;
-using ReadModel.Domain.Index.HttpRequest;
-using ReadModel.Domain.Index.Item;
-using ReadModel.Domain.Indexes;
 
 namespace ReadModel.Infrastructure.Clients
 {
@@ -16,11 +12,7 @@ namespace ReadModel.Infrastructure.Clients
         public ReadModelClient(ReadModelConfiguration readModelConfiguration)
         {
             var settings = new ConnectionSettings(new Uri(readModelConfiguration.Uri))
-                .BasicAuthentication(readModelConfiguration.Username, readModelConfiguration.Password)
-                .DefaultMappingFor<Item>(descriptor => descriptor.IndexName(IndexNameWithoutAlias.IndexName(IndexType.Item)))
-                .DefaultMappingFor<Item>(descriptor => descriptor.IndexName(IndexNameWithoutAlias.TemporaryIndexName(IndexType.Item)))
-                .DefaultMappingFor<HttpRequest>(descriptor => descriptor.IndexName(IndexNameWithoutAlias.IndexName(IndexType.HttpRequest)))
-                .DefaultMappingFor<HttpRequest>(descriptor => descriptor.IndexName(IndexNameWithoutAlias.TemporaryIndexName(IndexType.HttpRequest)));
+                .BasicAuthentication(readModelConfiguration.Username, readModelConfiguration.Password);
 
             ElasticClient = new ElasticClient(settings);
         }
